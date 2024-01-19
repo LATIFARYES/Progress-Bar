fefd110ed40068a5c8591c4e308bc38f9e702fc1
LATIFARYES <latifaryes@gmail.com>
2024-01-19 14:28:25 +0300

    Initial Commit

---
+++ progrees bar/progrees_barApp.swift
@@ -0,0 +1,17 @@
+//
+//  progrees_barApp.swift
+//  progrees bar
+//
+//  Created by LATIFA on 19/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct progrees_barApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
