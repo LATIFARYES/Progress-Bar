c2bf0f420db1d105a61271ecff655be55ed51a35
LATIFARYES <latifaryes@gmail.com>
2024-01-23 22:52:30 +0300

    1 - 3

--- progrees bar/ContentView.swift
+++ progrees bar/ContentView.swift
@@ -8,16 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+    
+    @State private var conttainerwidth: CGFloat = 0
+    @State private var progressTitle: String = " "
+    
+    
+    var maxWidth: Double {
+       return min((conttainerwidth / CGFloat(goal) * CGFloat(step)), conttainerwidth)
     }
-}
+    
+    private let goal = 3
+    @State private var step = 0
+    
+    var body: some View {
+        ZStack(alignment: .leading){
+            GeometryReader{ geo in
+                RoundedRectangle(cornerRadius: 60)
+                    .foregroundColor(.clear)
+                    .onAppear {
+                        conttainerwidth = geo.size.width
+                    }
+                
+                
+            }
+            
+           
+            RoundedRectangle(cornerRadius: 60)
+                .stroke(Color.bordercolor, lineWidth: 1)
+            
+            ZStack(alignment: .trailing){
+                
+                RoundedRectangle(cornerRadius: 60)
+                    .fill(Color.progresscolor)
+                
+                Text(progressTitle)
+                    .foregroundColor(.white)
+                    .padding(EdgeInsets(top: 6, leading: 12, bottom: 6, trailing: 12))
+                    .background(
+                        RoundedRectangle(cornerRadius: 60)
+                            .fill(Color.circlecolor)
+                    )
+            }
+            .padding(2)
+            .frame(minWidth: maxWidth)
+            .fixedSize()
+        }
+        .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+        .padding(20)
+        .onAppear {
+            progressTitle = " \(step) / \(goal)"
+        }
+        Button(" Start ") {
+            guard step < goal else { return }
+            
+            withAnimation {
+                step += 1
+            }
+         
+            progressTitle = " \(step) / \(goal)"
+        }
+        .tint(Color.circlecolor)
+    
+        }
+    }
+
+
+
 
 #Preview {
     ContentView()
